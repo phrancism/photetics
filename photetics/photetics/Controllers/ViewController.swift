8661ab4a22ff654c509b3a9d4d46ae0b00a46a0c
phrancism <francishmariano@gmail.com>
2020-03-29 13:46:04 -0700

    Add a new 'oral' representation (flower style)

--- photetics/photetics/Controllers/ViewController.swift
+++ photetics/photetics/Controllers/ViewController.swift
@@ -11,11 +11,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
 //      For an 'oral' representation
-//        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapViewHandler(_:)))
+//        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(speechStyleTapHandler(_:)))
 //        canvas.addGestureRecognizer(tapGestureRecognizer)
 
 //      For a 'written' representation'
 //        drawStanza()
+
+//      For another 'oral' representation
+        drawDots()
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(flowerStyleTapHandler(_:)))
+        canvas.addGestureRecognizer(tapGestureRecognizer)
     }
 
     private func drawStanza() {
@@ -31,13 +36,17 @@ class ViewController: UIViewController {
 
     private func makeColorFor(phoneString: String) -> UIColor {
         guard let phone = PhoneMap.default[phoneString],
-            let phoneHSL = phone as? HSBRepresentable
+            let phoneHSB = phone as? HSBRepresentable
             else { return Constants.defaultColor }
 
+        return makeColorFor(phone: phoneHSB)
+    }
+
+    private func makeColorFor(phone: HSBRepresentable) -> UIColor {
         return UIColor(
-            hue: phoneHSL.hue.cgFloat,
-            saturation: phoneHSL.saturation.cgFloat,
-            brightness: phoneHSL.brightness.cgFloat,
+            hue: phone.hue.cgFloat,
+            saturation: phone.saturation.cgFloat,
+            brightness: phone.brightness.cgFloat,
             alpha: 1
         )
     }
@@ -68,17 +77,17 @@ class ViewController: UIViewController {
     }
 
     @objc
-    func tapViewHandler(_ sender: UITapGestureRecognizer) {
+    func speechStyleTapHandler(_ sender: UITapGestureRecognizer) {
 
         let poemNoSpaces = poem.replacingOccurrences(of: " ", with: "")
         let colors: [UIColor] = poemNoSpaces.map { makeColorFor(phoneString: String($0)) }
 
         guard let view = sender.view else { return }
 
-        animateSpeechStyle(withColors: colors, onView: view)
+        animateSpeechStyleWith(colors: colors, onView: view)
     }
 
-    func animateSpeechStyle(withColors colors: [UIColor], onView view: UIView) {
+    func animateSpeechStyleWith(colors: [UIColor], onView view: UIView) {
         let overallDuration = 30.0
         let relativeDuration = colors.count.asDouble.reciprocal
         UIView.animateKeyframes(withDuration: overallDuration, delay: 0, options: [.calculationModePaced], animations: {
@@ -89,10 +98,70 @@ class ViewController: UIViewController {
                 }
         })
     }
+
+    @objc
+    func flowerStyleTapHandler(_ sender: UITapGestureRecognizer) {
+        let poemNoWhitespace = poem.replacingOccurrences(of: "\n", with: "").replacingOccurrences(of: " ", with: "")
+
+        guard let view = sender.view else { return }
+
+        animateFlowerStyleWith(text: poemNoWhitespace, onView: view)
+    }
+
+    func drawDots() {
+        let radius: CGFloat = Constants.flowerRadius
+        let strideIncrement = Constants.flowerAngleInterval
+
+        for (i, angle) in stride(from: -CGFloat.pi / 2, to: 3 * CGFloat.pi / 2, by: strideIncrement).enumerated() {
+            let positionX = (canvas.bounds.midX) + radius * cos(angle)
+            let positionY = (canvas.bounds.midY) + radius * sin(angle)
+            let dotViewRect = CGRect(
+                x: positionX,
+                y: positionY,
+                width: Constants.dotDiameter,
+                height: Constants.dotDiameter
+            )
+            let dotView = UIView(frame: dotViewRect)
+            dotView.backgroundColor = Constants.dotDefaultColor
+            dotView.layer.cornerRadius = Constants.dotRadius
+            canvas.addSubview(dotView)
+        }
+    }
+
+    func animateFlowerStyleWith(text: String, onView view: UIView) {
+        let overallDuration = 15.0
+        let relativeDuration = text.count.asDouble.reciprocal
+
+        UIView.animateKeyframes(withDuration: overallDuration, delay: 0, options: [.calculationModeLinear], animations: {
+            for (letterIndex, letter) in text.enumerated() {
+                guard let phone = PhoneMap.default[String(letter)],
+                    let phoneHSB = phone as? HSBRepresentable
+                    else { continue }
+
+                let color = self.makeColorFor(phone: phoneHSB)
+                let viewIndex = Int(phoneHSB.hue * 12)
+                let dotView = view.subviews[viewIndex]
+
+                UIView.addKeyframe(withRelativeStartTime: letterIndex.asDouble * relativeDuration,
+                                   relativeDuration: relativeDuration,
+                                   animations: { dotView.backgroundColor = color })
+
+                UIView.addKeyframe(withRelativeStartTime: (letterIndex.asDouble + 0.5) * relativeDuration,
+                                   relativeDuration: relativeDuration,
+                                   animations: { dotView.backgroundColor = Constants.dotDefaultColor })
+            }
+        })
+    }
 }
 
 extension ViewController {
     enum Constants {
+        static let dotDefaultColor: UIColor = .init(white: 0.2, alpha: 0.5)
+        static let dotPadding: CGFloat = 10.0
+        static let dotRadius: CGFloat = 20.0
+        static var dotDiameter: CGFloat { dotRadius * 2 }
+        static let flowerRadius: CGFloat = 150.0
+        static let flowerAngleInterval: CGFloat = 8 * CGFloat.pi / 44
         static let defaultColor: UIColor = .black
         static let horizontalPadding: CGFloat = 48.0
     }
